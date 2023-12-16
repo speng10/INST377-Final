fc86621b6cd12beb0a137ee35e096fb70ecda323
Shengjie <n9.shengjie@gmail.com>
2023-12-15 21:02:21 -0500

    Updated

--- public/homePage.js
+++ public/homePage.js
@@ -1,3 +1,90 @@
+const { createClient } = supabase
+const _supabase = createClient('https://lbqiwjxyjkqkortscmfx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxicWl3anh5amtxa29ydHNjbWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIwNzYwNjcsImV4cCI6MjAxNzY1MjA2N30.uyp1mO5freTCkRVHeGCV6ofvd6XuiXBdr9EgzbvQhe4');
+
+window.onload = async function () {
+    await fetchAndDisplayResults();
+
+    const mySubscription = _supabase
+        .from('userdemo')
+        .on('INSERT', payload => {
+            console.log('Change received!', payload);
+            // Fetch and display updated results after insert
+            fetchAndDisplayResults();
+        })
+        .subscribe();
+}
+
+async function searchAndInsert(type) {
+    const playerName = document.getElementById('playerName').value;
+    const teamName = document.getElementById('teamName').value;
+
+    // Check if the input is not empty
+    if ((type === 'player' && playerName.trim() === '') || (type === 'team' && teamName.trim() === '')) {
+        console.log('Invalid input or empty field');
+        return;
+    }
+
+    // Insert data into the database
+    const { data, error } = await _supabase
+        .from('userdemo')
+        .insert({ name: type === 'player' ? playerName : '', team: type === 'team' ? teamName : '' });
+
+    console.log('Insert Data:', data);
+    console.error('Insert Error:', error);
+
+    // Fetch and display updated results
+    await fetchAndDisplayResults();
+
+    // Fetch and display data from the API
+    if (type === 'player') {
+        await searchPlayer(playerName);
+    } else if (type === 'team') {
+        await searchTeam(teamName);
+    }
+}
+
+async function fetchAndDisplayResults() {
+
+    const { data: topPlayerData, error: playerError } = await _supabase
+        .from('userdemo')
+        .select('name', { count: 'exact', countColumns: ['name'] })
+        .not('name', 'is', null)
+        .not('name', 'eq', '')
+        .order('id', { ascending: false })
+        .range(0, 2);
+
+    const topPlayerTableBody = document.getElementById('topPlayerTableBody');
+    topPlayerTableBody.innerHTML = ''; 
+
+    topPlayerData.forEach(player => {
+        if (player.name) { // Check if name is not null or empty
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${player.name}</td>`;
+            topPlayerTableBody.appendChild(row);
+        }
+    });
+
+    // Fetch top team data
+    const { data: topTeamData, error: teamError } = await _supabase
+        .from('userdemo')
+        .select('team', { count: 'exact', countColumns: ['team'] })
+        .not('team', 'is', null)
+        .not('team', 'eq', '')
+        .order('id', { ascending: false })
+        .range(0, 2);
+
+    const topTeamTableBody = document.getElementById('topTeamTableBody');
+    topTeamTableBody.innerHTML = ''; 
+
+    topTeamData.forEach(team => {
+        if (team.team) { // Check if team is not null or empty
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${team.team}</td>`;
+            topTeamTableBody.appendChild(row);
+        }
+    });
+}
+
 async function searchPlayer() {
     const playerName = document.getElementById('playerName').value;
     const response = await fetch(`https://www.balldontlie.io/api/v1/players?search=${playerName}`);
